ff440e991223d5cf56b979e69f75ab186979963e
TurkHack <131433860+TurkHacker123@users.noreply.github.com>
2023-11-25 11:28:02 +0300

    Create net.go

---
+++ util/net.go
@@ -0,0 +1,54 @@
+package util
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+var ErrBadStatus = errors.New("Bad Status, Sorry about that")
+
+func Download(url, file string) error {
+	out, err := os.Create(file)
+	if err != nil {
+		return err
+	}
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("%w: %s", ErrBadStatus, resp.Status)
+	}
+
+	_, err = io.Copy(out, resp.Body)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func Body(url string) (string, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return "", fmt.Errorf("%w: %s", ErrBadStatus, resp.Status)
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	return string(body), nil
+}
